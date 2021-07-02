34078a61e24524eda3d89c3d927c3a9176201217
Karsten Tausche <karsten@fairphone.com>
2021-07-02 15:38:10 +0200

    sign_target_files_apks: update ro.build.keys
    CAF introduces ro.build.keys as build property, see change
    I4182ba2e297d69ddd203f7e6ef0905e1e7239eaa here and
    I9f06bd8259613ad6cf93fe990cb27b06f5514937 in system/core. However, it
    doesn't seem to support signing target files. We keep the release keys
    out of the build and only sign the target files with them, so we also
    need to update all related system properties during signing (as
    recommended and implemented by AOSP).
    
    Issue: FP3-A11#54
    Issue: FP4-INT#19
    Issue: FP4-A14#27
    Issue: FP4-A14#193
    Change-Id: Ibca057e95a53b7ad7a4dad9948f93992716a1a96
    (cherry picked from commit 25f33842270b71f489d05c740d3fd205f77b881a)
    (cherry picked from commit 3f6c610206383e94d5a9a63c48abbab052be27d5)
    (cherry picked from commit d41cf6f24865a11de5a0f12154ce0e6f2bff851f)

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -1042,6 +1042,8 @@ def RewriteProps(data):
         if len(value) > 1 and value[-1].endswith("-keys"):
           value.pop()
         value = " ".join(value)
+      elif key == "ro.build.keys":
+        value = EditTags(value)
       line = key + "=" + value
     if line != original_line:
       print("  replace: ", original_line)
