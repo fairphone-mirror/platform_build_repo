8685d8ab9725a55ba718c8b8c27607196a5ff138
android-t2 <xiaocong.gu@t2mobile.com>
2023-02-16 14:56:57 +0800

    [FP5-103]build properties values
    Change-Id: Ia5ff91e8abb24aee0c90fd9a44f608634e5e6ba8

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -36,10 +36,10 @@ define generate-common-build-props
     echo "####################################" >> $(2);\
     $(if $(filter system,$(1)),\
         echo "ro.product.$(1).brand=$(PRODUCT_SYSTEM_BRAND)" >> $(2);\
-        echo "ro.product.$(1).device=$(PRODUCT_SYSTEM_DEVICE)" >> $(2);\
+        echo "ro.product.$(1).device=$(OVERRIDE_TARGET_DEVICE)" >> $(2);\
         echo "ro.product.$(1).manufacturer=$(PRODUCT_SYSTEM_MANUFACTURER)" >> $(2);\
         echo "ro.product.$(1).model=$(PRODUCT_SYSTEM_MODEL)" >> $(2);\
-        echo "ro.product.$(1).name=$(PRODUCT_SYSTEM_NAME)" >> $(2);\
+        echo "ro.product.$(1).name=$(OVERRIDE_TARGET_PRODUCT)" >> $(2);\
       ,\
         echo "ro.product.$(1).brand=$(PRODUCT_BRAND)" >> $(2);\
         echo "ro.product.$(1).device=$(TARGET_DEVICE)" >> $(2);\
@@ -176,7 +176,11 @@ ifeq (,$(strip $(BUILD_FINGERPRINT)))
   else
     BF_BUILD_NUMBER := $(file <$(BUILD_NUMBER_FILE))
   endif
-  BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(TARGET_PRODUCT)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BF_BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/$(BUILD_VERSION_TAGS)
+  ifneq ($(strip $(OVERRIDE_TARGET_PRODUCT)),)
+    BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(OVERRIDE_TARGET_PRODUCT)/$(OVERRIDE_TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BF_BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/$(BUILD_VERSION_TAGS)
+  else
+    BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(TARGET_PRODUCT)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BF_BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/$(BUILD_VERSION_TAGS)
+  endif
 endif
 # unset it for safety.
 BF_BUILD_NUMBER :=
@@ -279,6 +283,8 @@ $(gen_from_buildinfo_sh): $(INTERNAL_BUILD_ID_MAKEFILE) $(API_FINGERPRINT) | $(B
 	        TARGET_CPU_ABI2="$(TARGET_CPU_ABI2)" \
 	        ZYGOTE_FORCE_64_BIT="$(ZYGOTE_FORCE_64_BIT)" \
 	        BUILD_DISPLAY_ID="$(BUILD_DISPLAY_ID)" \
+	        OVERRIDE_TARGET_DEVICE="$(OVERRIDE_TARGET_DEVICE)" \
+	        OVERRIDE_TARGET_PRODUCT="$(OVERRIDE_TARGET_PRODUCT)" \
 	        bash $(BUILDINFO_SH) > $@
 
 ifdef TARGET_SYSTEM_PROP
