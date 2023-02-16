55ff79a7e13dfeb7f11f2ee61781b075eaac60e4
android-t2 <xiaocong.gu@t2mobile.com>
2023-02-16 14:56:57 +0800

    [FP5-103]build properties values
    Change-Id: Ia5ff91e8abb24aee0c90fd9a44f608634e5e6ba8

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -8,9 +8,9 @@ datetime=$(date +'%Y%m%d')
 display_version=`awk -F\" '/BUILD_AP_VER/{print $2}' version/build_version.h`
 
 if [ "$TARGET_BUILD_VARIANT" = "user" ]; then
-    BUILD_DISPLAY_ID=FP4.$BUILD_NUMBER.$display_version.$datetime
+    BUILD_DISPLAY_ID=FP5.$BUILD_NUMBER.$display_version.$datetime
 else
-    BUILD_DISPLAY_ID=FP4.$BUILD_NUMBER.$display_version-userdebug.$datetime
+    BUILD_DISPLAY_ID=FP5.$BUILD_NUMBER.$display_version-userdebug.$datetime
 fi
 
 echo "ro.build.display.id=$BUILD_DISPLAY_ID"
