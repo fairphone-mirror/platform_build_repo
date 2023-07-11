b893aef77545a355f1b7503cf18b02e895737ded
chuanzhi.shao <chuanzhi.shao@t2mobile.com>
2023-07-11 15:48:47 +0800

    [1/4][FP5-2241]Onboarding forum: Add Fairphone ringtone 'Sunbeam' to ringtones
    * Root Cause
      new demand
    * Solution
      'Sunbeam' should be a selectable ringtone
    * Test Steps
      Go to settings->Sound & vibration->Phone ringtone
    * Test Result
      pass
    
    Change-Id: I406493c680c6c6a128164c5ea3aa5b428338d055

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += \
 
 # Additional settings used in all AOSP builds
 PRODUCT_VENDOR_PROPERTIES := \
-    ro.config.ringtone?=Ring_Synth_04.ogg \
+    ro.config.ringtone?=Sunbeam.mp3 \
     ro.config.notification_sound?=pixiedust.ogg
 
 # Put en_US first in the list, so make it default.
