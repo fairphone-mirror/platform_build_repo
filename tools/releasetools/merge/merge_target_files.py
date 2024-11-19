2b9381d9b6fed6d5ef1fad228d0118c19a510c3b
liquan.zhou <liquan.zhou@t2mobile.com>
2024-11-19 17:44:35 +0800

    FP5V-159 [1]OTA (GOTA) support
    * Comment: Generate OTA package with release images and target_files_extract.zip.
    * Root Cause: Requirement
    * Solution: Porting code from FP5-294.
    * Test Steps:
        1. build completed successfully
        2. check if out/target/product/lahaina/target_files_extract.zip exists
        3. generate target_files package and ota package
        4. check ota package
    * Test Result: Pass
    
    Change-Id: I50d6011b197912fd01f7ec0a9dd5fcabb9b08c91

--- tools/releasetools/merge/merge_target_files.py
+++ tools/releasetools/merge/merge_target_files.py
@@ -162,7 +162,9 @@ OPTIONS.allow_partial_ab = False
 OPTIONS.framework_dexpreopt_config = None
 OPTIONS.framework_dexpreopt_tools = None
 OPTIONS.vendor_dexpreopt_config = None
-
+#+FP5-294. Generate target_files_extract.zip for FOTA.
+OPTIONS.t2m_target_files_extarct_build = False
+#-FP5-294. Generate target_files_extract.zip for FOTA.
 
 def move_only_exists(source, destination):
   """Judge whether the file exists and then move the file."""
@@ -470,36 +472,37 @@ def merge_target_files(temp_dir):
 
   output_target_files_temp_dir = create_merged_package(temp_dir)
 
-  partition_map = common.PartitionMapFromTargetFiles(
-      output_target_files_temp_dir)
+  if not OPTIONS.t2m_target_files_extarct_build:
+    partition_map = common.PartitionMapFromTargetFiles(
+        output_target_files_temp_dir)
 
-  compatibility_errors = merge_compatibility_checks.CheckCompatibility(
-      target_files_dir=output_target_files_temp_dir,
-      partition_map=partition_map)
-  if compatibility_errors:
-    for error in compatibility_errors:
-      logger.error(error)
-    raise ExternalError(
-        'Found incompatibilities in the merged target files package.')
+    compatibility_errors = merge_compatibility_checks.CheckCompatibility(
+        target_files_dir=output_target_files_temp_dir,
+        partition_map=partition_map)
+    if compatibility_errors:
+      for error in compatibility_errors:
+        logger.error(error)
+      raise ExternalError(
+          'Found incompatibilities in the merged target files package.')
 
-  # Include the compiled policy in an image if requested.
-  if OPTIONS.rebuild_sepolicy:
-    rebuild_image_with_sepolicy(output_target_files_temp_dir)
+    # Include the compiled policy in an image if requested.
+    if OPTIONS.rebuild_sepolicy:
+      rebuild_image_with_sepolicy(output_target_files_temp_dir)
 
-  generate_missing_images(output_target_files_temp_dir)
+    generate_missing_images(output_target_files_temp_dir)
 
-  generate_super_empty_image(output_target_files_temp_dir,
-                             OPTIONS.output_super_empty)
+    generate_super_empty_image(output_target_files_temp_dir,
+                               OPTIONS.output_super_empty)
 
-  # Finally, create the output target files zip archive and/or copy the
-  # output items to the output target files directory.
+    # Finally, create the output target files zip archive and/or copy the
+    # output items to the output target files directory.
 
-  if OPTIONS.output_dir:
-    merge_utils.CopyItems(output_target_files_temp_dir, OPTIONS.output_dir,
-                          OPTIONS.output_item_list)
+    if OPTIONS.output_dir:
+      merge_utils.CopyItems(output_target_files_temp_dir, OPTIONS.output_dir,
+                            OPTIONS.output_item_list)
 
-  if not OPTIONS.output_target_files:
-    return
+    if not OPTIONS.output_target_files:
+      return
 
   create_target_files_archive(OPTIONS.output_target_files,
                               output_target_files_temp_dir, temp_dir)
@@ -589,6 +592,10 @@ def main():
       OPTIONS.framework_dexpreopt_tools = a
     elif o == '--vendor-dexpreopt-config':
       OPTIONS.vendor_dexpreopt_config = a
+    #+FP5-294. Generate target_files_extract.zip for FOTA. liquan.zhou.t2m. 20230416
+    elif o == '--t2m_target_files_extarct_build':
+      OPTIONS.t2m_target_files_extarct_build = True
+    #-FP5-294. Generate target_files_extract.zip for FOTA. liquan.zhou.t2m. 20230416
     else:
       return False
     return True
@@ -623,6 +630,7 @@ def main():
           'vendor-otatools=',
           'rebuild-sepolicy',
           'keep-tmp',
+          't2m_target_files_extarct_build', #FP5-294. Generate target_files_extract.zip for FOTA. liquan.zhou.t2m. 20230416
           'avb-resolve-rollback-index-location-conflict',
           'allow-partial-ab',
       ],
