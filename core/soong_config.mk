12198e4af972f35122d79feaeceb2df3dcc9b7f6
Yi FAN <yi.fan@t2mobile.com>
2025-02-07 14:10:12 +0800

    [FPS-129] [FPS]Software version identifiers
    * Root Cause
      Software version identifiers
    * Solution
      ro.build.display.id customization
    * Test Steps
      [PASS] check version display
    * Bug Source Type
      Development
    
    Change-Id: I6c4de222fe93bea1e96faa1e7e2a42691ff58173

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -75,6 +75,8 @@ $(call add_json_str,  DeviceSecondaryArchVariant,        $(TARGET_2ND_ARCH_VARIA
 $(call add_json_str,  DeviceSecondaryCpuVariant,         $(TARGET_2ND_CPU_VARIANT))
 $(call add_json_list, DeviceSecondaryAbi,                $(TARGET_2ND_CPU_ABI) $(TARGET_2ND_CPU_ABI2))
 
+$(call add_json_str,  Fairphone_rel_type,                $(FAIRPHONE_REL_TYPE))
+
 $(call add_json_bool, Aml_abis,                          $(if $(filter mainline_sdk,$(TARGET_ARCH_SUITE)),true))
 $(call add_json_bool, Ndk_abis,                          $(if $(filter ndk,         $(TARGET_ARCH_SUITE)),true))
 
