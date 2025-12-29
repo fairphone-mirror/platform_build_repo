3f1ee33670201e277737c5ebcf9e61467bfc69bd
jenkins-agent <jenkins-agent>
2025-12-29 16:22:45 +0100

    Bump build id to FP4.SREL.15.14.4
    Change-Id: Iea3d6be77a66b37415231f079e5b3c9176db11a0

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=FP4.QREL.15.14.3
+BUILD_ID=FP4.SREL.15.14.4
 PLATFORM_BASE_OS=Fairphone/FP4eea/FP4:15/FP4.QREL.15.14.3/15.14.3:user/release-keys
